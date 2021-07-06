b31bd1cf4c865977ddcb67db1deb9b56b1abbc2b
iusharnina@gmail.com <zjulia679@gmail.com>
2021-07-07 09:58:06 +1000

    Practice variables

---
+++ user_input.py
@@ -0,0 +1,9 @@
+answer = input("How are you? ")
+print(answer)
+
+message = f"I'm {answer} as well, thanks"
+print(message)
+
+name = input("What is your name? ")
+hobby = input("What is your hobby ")
+print(f"This is {name}, like {hobby}")
---
+++ variables.py
@@ -0,0 +1,63 @@
+
+#Variables
+dog = "Jett"
+#print(dog)
+#print(type(dog))
+
+julia = f"Hey, my dog's name is {dog}"
+#print(julia)
+
+day = "Saturday"
+message = f"Today is {day}"
+#print(message)
+#print(type(day))
+
+month = "November"
+message = f"This is a {day} in {month}"
+#print(message)
+#print(type(month))
+
+#Integer Float
+apples = 5
+bananas = 8
+fruits = apples + bananas
+#print(fruits)
+
+height = 1.70
+weight = 999.9
+calculation = weight - height
+#print(calculation)
+
+
+#Integers
+run1 = 1400
+run2 = 1800
+
+total = run1 + run2 
+print(total)
+
+#Floats
+run3 = 1.7
+run4 = 2.4
+total = run3 / run4
+print(total)
+
+goal_distance = 10900
+run1_left = goal_distance - run1
+print(run1_left)
+
+
+var1 = "banana"
+var2 = "3"
+var3 = 5
+
+var4 = 10
+
+calculation = var4 / var3
+print(calculation)
+
+run5 = "5000"
+
+print(run5 + "3")
+print(run5 * 3)
+print(run5 * 3.0)
