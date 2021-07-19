0f0015c222e028129f182f9f2f77c6c80fe57082
iusharnina@gmail.com <zjulia679@gmail.com>
2021-07-19 16:17:42 +1000

    Fix data exercise

--- data/process_employee_birthday.py
+++ data/process_employee_birthday.py
@@ -1,13 +1,13 @@
 import csv
 
-with open('employee_birthday.csv', mode="r") as csv_file:
+with open('data\data1\employee_birthday.csv', mode="r") as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=",")
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
-            line_count += 1
-            print(
-                f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
-            line_count += 1
-            print(f"Processed {line_count} lines.")
+
+        print(
+            f'\t{row["name"]} works in the {row[" department"]} department, and was born in {row[" birthday month"]}.')
+        line_count += 1
+    print(f"Processed {line_count} lines.")
